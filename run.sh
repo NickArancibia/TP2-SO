1a94c446538d6e9b03b993ffc37c73f981b1bd47
Nick <narancibiacarabajal@gmail.com>
2024-09-22 23:31:01 -0300

    Add nosound execution

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+if [ "$1" == "nosound" ]; then
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+else
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+fi
